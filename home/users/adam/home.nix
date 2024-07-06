cb2fe2dc092174fd7d61af7829b79b7926c6845f
cnst <cnst@cana.st>
2024-07-06 08:40:55 +0200

    simplify

--- home/users/adam/home.nix
+++ home/users/adam/home.nix
@@ -9,19 +9,19 @@
 }: {
   # You can import other home-manager modules here
   imports = [
-    ../core/adam.nix
-    ../extra/adam.nix
+    ../../core/adam.nix
+    ../../extra/adam.nix
   ];
 
-  nixpkgs = {
-    # You can add overlays here
-    overlays = [];
-    # Configure your nixpkgs instance
-    config = {
-      # Disable if you don't want unfree packages
-      allowUnfree = true;
-      # Workaround for https://github.com/nix-community/home-manager/issues/2942
-      allowUnfreePredicate = _: true;
+  nix = {
+    package = lib.mkDefault pkgs.nix;
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+        "ca-derivations"
+      ];
+      warn-dirty = false;
     };
   };
 
--- home/users/cnst/home.nix
+++ home/users/cnst/home.nix
@@ -13,15 +13,15 @@
     ../../extra/cnst.nix
   ];
 
-  nixpkgs = {
-    # You can add overlays here
-    overlays = [];
-    # Configure your nixpkgs instance
-    config = {
-      # Disable if you don't want unfree packages
-      allowUnfree = true;
-      # Workaround for https://github.com/nix-community/home-manager/issues/2942
-      allowUnfreePredicate = _: true;
+  nix = {
+    package = lib.mkDefault pkgs.nix;
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+        "ca-derivations"
+      ];
+      warn-dirty = false;
     };
   };
 
