b62539a79976809d81885ad58c23e55a7f11b038
cnst <cnst@cana.st>
2024-07-07 10:35:54 +0200

    some resturcturing of hm 5

--- home/users/cnst/imports.nix
+++ home/users/cnst/imports.nix
@@ -2,7 +2,11 @@
   imports = [
     # CORE
     # .gui
-    ../../core/gui
+    ../../core/gui/anyrun
+    ../../core/gui/gtk
+    ../../core/gui/mako
+    ../../core/gui/waybar
+    ../../core/gui/xdg
     # .tui
     ../../core/tui/git/cnst.nix
     ../../core/tui/shell/cnst.nix
@@ -16,13 +20,43 @@
   ];
   home = {
     packages = with pkgs; [
-      # .applications
-      alacritty
+      # MISCELLANEOUS
+      # .gui
+      gnome.file-roller
+      gnome.gnome-calculator
       keepassxc
+      nwg-look
+      oculante
+      pavucontrol
       qbittorrent
-      webcord
-      calcurse
       virt-manager
+      webcord
+      xfce.thunar
+
+      # .tui
+      alacritty
+      btop
+      calcurse
+      fastfetch
+      htop
+      ranger
+
+      # .system
+      gnome.adwaita-icon-theme
+      grimblast
+      hyprpicker
+      networkmanagerapplet
+      pamixer
+      qt5.qtwayland
+      qt6.qtwayland
+      slurp
+      swaybg
+      thefuck
+      wireguard-tools
+      wl-clipboard
+      wpa_supplicant
+      xfce.thunar-archive-plugin
+      xfce.thunar-volman
     ];
     sessionVariables = {
       BROWSER = "firefox";
