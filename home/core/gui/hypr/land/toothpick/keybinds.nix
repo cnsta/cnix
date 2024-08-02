70c6cd7ab7b0bd77343a7673df62b3e827ba5e45
cnst <adamhilmersson@gmail.com>
2024-08-02 15:36:07 +0200

    some changes to toothpick settings

--- home/core/gui/hypr/land/toothpick/keybinds.nix
+++ home/core/gui/hypr/land/toothpick/keybinds.nix
@@ -11,60 +11,60 @@
     "$ranger" = "rangerscript";
 
     # See https://wiki.hyprland.org/Configuring/Keywords/ for more
-    "$mainMod" = "SUPER";
+    "$mod" = "ALT_L";
 
     bind = [
       # Custom binds
-      "SUPER SHIFT, B, exec, pkill -SIGUSR2 waybar" # Reload waybar
+      "$mod SHIFT, B, exec, pkill -SIGUSR2 waybar" # Reload waybar
 
       # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
-      "SUPER, A, exec, pkill -SIGUSR1 waybar"
-      "SUPER, T, exec, $terminal"
-      "SUPER, W, exec, $browser"
-      "SUPER, K, exec, $passwordManager"
-      "SUPER SHIFT, W, exec, $browserinc"
-      "SUPER, Q, killactive,"
-      #bind = SUPER, M, exec, hyprctl dispatch exit
-      #bind = SUPER, E, exec, $fileManager
-      "SUPER, E, exec, $fileManager"
-      "SUPER SHIFT, E, exec, $ranger"
-      "SUPER, F, fullscreen,"
-      "SUPER SHIFT, F, togglefloating,"
-      "SUPER, SPACE, exec, $menu"
-      "SUPER, P, pseudo," # dwindle
-      "SUPER, J, togglesplit," # dwindle
-      "SUPER, C, exec, hyprctl dispatch exec copyq toggle"
-      "SUPER, TAB, exec, $menuw"
+      "$mod, A, exec, pkill -SIGUSR1 waybar"
+      "$mod, T, exec, $terminal"
+      "$mod, W, exec, $browser"
+      "$mod, K, exec, $passwordManager"
+      "$mod SHIFT, W, exec, $browserinc"
+      "$mod, Q, killactive,"
+      #bind = $mod, M, exec, hyprctl dispatch exit
+      #bind = $mod, E, exec, $fileManager
+      "$mod, E, exec, $fileManager"
+      "$mod SHIFT, E, exec, $ranger"
+      "$mod, F, fullscreen,"
+      "$mod SHIFT, F, togglefloating,"
+      "$mod, SPACE, exec, $menu"
+      "$mod, P, pseudo," # dwindle
+      "$mod, J, togglesplit," # dwindle
+      "$mod, C, exec, hyprctl dispatch exec copyq toggle"
+      "$mod, TAB, exec, $menuw"
 
       # Move focus with mainMod + arrow keys
-      "SUPER, left, movefocus, l"
-      "SUPER, right, movefocus, r"
-      "SUPER, up, movefocus, u"
-      "SUPER, down, movefocus, d"
+      "$mod, left, movefocus, l"
+      "$mod, right, movefocus, r"
+      "$mod, up, movefocus, u"
+      "$mod, down, movefocus, d"
 
       # Switch workspaces with mainMod + [0-9]
-      "SUPER, 1, workspace, 1"
-      "SUPER, 2, workspace, 2"
-      "SUPER, 3, workspace, 3"
-      "SUPER, 4, workspace, 4"
-      "SUPER, 5, workspace, 5"
-      "SUPER, 6, workspace, 6"
-      "SUPER, 7, workspace, 7"
-      "SUPER, 8, workspace, 8"
-      "SUPER, 9, workspace, 9"
-      "SUPER, 0, workspace, 10"
+      "$mod, 1, workspace, 1"
+      "$mod, 2, workspace, 2"
+      "$mod, 3, workspace, 3"
+      "$mod, 4, workspace, 4"
+      "$mod, 5, workspace, 5"
+      "$mod, 6, workspace, 6"
+      "$mod, 7, workspace, 7"
+      "$mod, 8, workspace, 8"
+      "$mod, 9, workspace, 9"
+      "$mod, 0, workspace, 10"
 
       # Move active window to a workspace with mainMod + SHIFT + [0-9]
-      "SUPER SHIFT, 1, movetoworkspace, 1"
-      "SUPER SHIFT, 2, movetoworkspace, 2"
-      "SUPER SHIFT, 3, movetoworkspace, 3"
-      "SUPER SHIFT, 4, movetoworkspace, 4"
-      "SUPER SHIFT, 5, movetoworkspace, 5"
-      "SUPER SHIFT, 6, movetoworkspace, 6"
-      "SUPER SHIFT, 7, movetoworkspace, 7"
-      "SUPER SHIFT, 8, movetoworkspace, 8"
-      "SUPER SHIFT, 9, movetoworkspace, 9"
-      "SUPER SHIFT, 0, movetoworkspace, 10"
+      "$mod SHIFT, 1, movetoworkspace, 1"
+      "$mod SHIFT, 2, movetoworkspace, 2"
+      "$mod SHIFT, 3, movetoworkspace, 3"
+      "$mod SHIFT, 4, movetoworkspace, 4"
+      "$mod SHIFT, 5, movetoworkspace, 5"
+      "$mod SHIFT, 6, movetoworkspace, 6"
+      "$mod SHIFT, 7, movetoworkspace, 7"
+      "$mod SHIFT, 8, movetoworkspace, 8"
+      "$mod SHIFT, 9, movetoworkspace, 9"
+      "$mod SHIFT, 0, movetoworkspace, 10"
 
       # Laptop controls
       ",XF86AudioLowerVolume, exec, pamixer -d 5"
@@ -74,28 +74,28 @@
       ",XF86MonBrightnessDown, exec, brightnessctl s 10%-"
       ",XF86MonBrightnessUp, exec, brightnessctl s +10%"
 
-      "SUPER, XF86MonBrightnessUp, exec, hyprctl dispatch dpms on"
-      "SUPER, XF86MonBrightnessDown, exec, hyprctl dispatch dpms off"
+      "$mod, XF86MonBrightnessUp, exec, hyprctl dispatch dpms on"
+      "$mod, XF86MonBrightnessDown, exec, hyprctl dispatch dpms off"
 
       # Screenshot a window
-      "SUPER, F10, exec, hyprshot -m window"
+      "$mod, F10, exec, hyprshot -m window"
       # Screenshot a monitor
       ", F10, exec, hyprshot -m output"
       # Screenshot a region
-      "SUPER SHIFT, F10, exec, hyprshot -m region"
+      "$mod SHIFT, F10, exec, hyprshot -m region"
 
       # Example special workspace (scratchpad)
-      "SUPER, S, togglespecialworkspace, magic"
-      "SUPER SHIFT, S, movetoworkspace, special:magic"
+      "$mod, S, togglespecialworkspace, magic"
+      "$mod SHIFT, S, movetoworkspace, special:magic"
 
       # Scroll through existing workspaces with mainMod + scroll
-      # bind = SUPER, mouse_down, workspace, e+1
-      # bind = SUPER, mouse_up, workspace, e-1
+      # bind = $mod, mouse_down, workspace, e+1
+      # bind = $mod, mouse_up, workspace, e-1
     ];
     # Move/resize windows with mainMod + LMB/RMB and dragging
     bindm = [
-      "SUPER, mouse:272, movewindow"
-      "SUPER, mouse:273, resizewindow"
+      "$mod, mouse:272, movewindow"
+      "$mod, mouse:273, resizewindow"
     ];
   };
 }
