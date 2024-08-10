0cc0cd6984b15e245d48d834485954d8c4a3caf6
cnst <adamhilmersson@gmail.com>
2024-08-10 20:37:22 +0200

    changes to firefox and some other things

--- home/opt/browsers/firefox/default.nix
+++ home/opt/browsers/firefox/default.nix
@@ -6,26 +6,28 @@
   programs.firefox = {
     enable = true;
     package = inputs.firefox-nightly.packages.${pkgs.system}.firefox-nightly-bin;
-    profiles.cnst = {
-      search = {
-        force = true;
-        default = "DuckDuckGo";
-        privateDefault = "DuckDuckGo";
-        order = ["DuckDuckGo" "Google"];
-      };
-      bookmarks = {};
-      extensions = with inputs.firefox-addons.packages.${pkgs.system}; [
-        ublock-origin
-        sponsorblock
-        clearurls
-        return-youtube-dislikes
-        # enhancer-for-youtube # unfree
-      ];
-      settings = {
-        "apz.overscroll.enabled" = true;
-        "browser.aboutConfig.showWarning" = false;
-        "general.autoScroll" = true;
-        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+    profiles = {
+      default = {
+        search = {
+          force = true;
+          default = "DuckDuckGo";
+          privateDefault = "DuckDuckGo";
+          order = ["DuckDuckGo" "Google"];
+        };
+        bookmarks = {};
+        extensions = with inputs.firefox-addons.packages.${pkgs.system}; [
+          ublock-origin
+          sponsorblock
+          clearurls
+          return-youtube-dislikes
+          # enhancer-for-youtube # unfree
+        ];
+        settings = {
+          "apz.overscroll.enabled" = true;
+          "browser.aboutConfig.showWarning" = false;
+          "general.autoScroll" = true;
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        };
       };
     };
   };
