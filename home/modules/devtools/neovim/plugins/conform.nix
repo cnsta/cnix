1be8ec939b8a0389092fabd7874db9eb7e75bf10
cnst <adamhilmersson@gmail.com>
2024-09-15 18:27:29 +0200

    hyprland runix changes

--- home/modules/devtools/neovim/plugins/conform.nix
+++ home/modules/devtools/neovim/plugins/conform.nix
@@ -11,9 +11,9 @@
           require("conform").setup({
           	default_format_opts = {
           		timeout_ms = 3000,
-          		async = false, -- not recommended to change
-          		quiet = false, -- not recommended to change
-          		lsp_format = "fallback", -- not recommended to change
+          		async = false,
+          		quiet = false,
+          		lsp_format = "fallback",
           	},
           	formatters_by_ft = {
           		bash = { "shfmt" },
@@ -23,8 +23,8 @@
           		json = { "fixjson" },
           		lua = { "stylua" },
           		nix = { "alejandra" },
-          		php = { "php_cs_fixer" },
-           		python = { "black" },
+             		php = { "php_cs_fixer" },
+          		python = { "black" },
           		rust = { "rustfmt" },
           		sh = { "shfmt" },
           		typescript = { "prettierd" },
@@ -32,14 +32,6 @@
           		yaml = { "prettierd" },
           		["*"] = { "injected" },
           	},
-          })
-
-          -- Optionally, set up a command or auto-command to format on save
-          vim.api.nvim_create_autocmd("BufWritePre", {
-          	pattern = "*",
-          	callback = function()
-          		require("conform").format()
-          	end,
           })
         '';
     }
