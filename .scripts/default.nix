331266c368d15d9160354cdd7ad13ef8c2d8569c
cnst <adamhilmersson@gmail.com>
2024-10-22 19:16:59 +0200

    rm pointless comments

--- .scripts/default.nix
+++ .scripts/default.nix
@@ -9,53 +9,50 @@
   inherit (builtins) readFile;
 in {
   home = {
-    # Ensure the scripts are linked in the session PATH
     sessionPath = ["${config.home.homeDirectory}/.local/bin"];
 
-    # Link the pavucontrol script to the local PATH
     file = {
       ".local/bin/pavucontrol-toggle.sh" = {
         source = getExe (pkgs.writeShellApplication {
           name = "pavucontrol-toggle";
-          runtimeInputs = with pkgs; [hyprland]; # Add any required runtime dependencies here
-          text = readFile ./bin/pavucontrol-toggle.sh; # Path to your pavucontrol.sh script
+          runtimeInputs = with pkgs; [hyprland];
+          text = readFile ./bin/pavucontrol-toggle.sh;
         });
       };
 
       ".local/bin/tuirun-toggle.sh" = {
         source = getExe (pkgs.writeShellApplication {
           name = "tuirun-toggle";
-          runtimeInputs = with pkgs; [hyprland]; # Add any required runtime dependencies here
-          text = readFile ./bin/tuirun-toggle.sh; # Path to your tuirun-toggle.sh script
+          runtimeInputs = with pkgs; [hyprland];
+          text = readFile ./bin/tuirun-toggle.sh;
         });
       };
 
       ".local/bin/tuirun-debug.sh" = {
         source = getExe (pkgs.writeShellApplication {
           name = "tuirun-debug";
-          runtimeInputs = with pkgs; [hyprland]; # Add any required runtime dependencies here
-          text = readFile ./bin/tuirun-debug.sh; # Path to your tuirun-toggle.sh script
+          runtimeInputs = with pkgs; [hyprland];
+          text = readFile ./bin/tuirun-debug.sh;
         });
       };
 
       ".local/bin/calcurse-toggle.sh" = {
         source = getExe (pkgs.writeShellApplication {
           name = "calcurse-toggle";
-          runtimeInputs = with pkgs; [hyprland]; # Add any required runtime dependencies here
-          text = readFile ./bin/calcurse-toggle.sh; # Path to your calcurse-toggle.sh script
+          runtimeInputs = with pkgs; [hyprland];
+          text = readFile ./bin/calcurse-toggle.sh;
         });
       };
 
       ".local/bin/volume-control.sh" = {
         source = getExe (pkgs.writeShellApplication {
           name = "volume-control";
-          runtimeInputs = with pkgs; [pamixer libnotify]; # Add any required runtime dependencies here
-          text = readFile ./bin/volume-control.sh; # Path to your volume-control.sh script
+          runtimeInputs = with pkgs; [pamixer libnotify];
+          text = readFile ./bin/volume-control.sh;
         });
       };
 
       ".local/bin/extract.sh" = {
-        # Stolen from NotAShelf
         source = getExe (pkgs.writeShellApplication {
           name = "extract";
           runtimeInputs = with pkgs; [zip unzip gnutar p7zip];
