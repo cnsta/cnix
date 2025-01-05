c5588244d363f5b3bc27fc9cc41f055e7469ac1d
cnst <adam@cnst.dev>
2025-01-05 15:13:10 +0100

    gtk fixes to ensure no border radii etc

--- modules/home/services/gtk/default.nix
+++ modules/home/services/gtk/default.nix
@@ -56,45 +56,55 @@ in {
         '';
       };
 
-      gtk3.extraConfig = {
-        # Lets be easy on the eyes. This should be easy to make dependent on
-        # the "variant" of the theme, but I never use a light theme anyway.
-        gtk-application-prefer-dark-theme = true;
+      gtk3 = {
+        extraConfig = {
+          # Lets be easy on the eyes. This should be easy to make dependent on
+          # the "variant" of the theme, but I never use a light theme anyway.
+          gtk-application-prefer-dark-theme = true;
 
-        # Decorations
-        gtk-decoration-layout = "appmenu:none";
-        gtk-toolbar-style = "GTK_TOOLBAR_BOTH";
-        gtk-toolbar-icon-size = "GTK_ICON_SIZE_LARGE_TOOLBAR";
-        gtk-button-images = 1;
-        gtk-menu-images = 1;
+          # Decorations
+          gtk-decoration-layout = "appmenu:none";
+          gtk-toolbar-style = "GTK_TOOLBAR_BOTH";
+          gtk-toolbar-icon-size = "GTK_ICON_SIZE_LARGE_TOOLBAR";
+          gtk-button-images = 1;
+          gtk-menu-images = 1;
 
-        # Silence bells and whistles, quite literally.
-        gtk-error-bell = 0;
-        gtk-enable-event-sounds = 0;
-        gtk-enable-input-feedback-sounds = 0;
+          # Silence bells and whistles, quite literally.
+          gtk-error-bell = 0;
+          gtk-enable-event-sounds = 0;
+          gtk-enable-input-feedback-sounds = 0;
 
-        # Fonts
-        gtk-xft-antialias = 1;
-        gtk-xft-hinting = 1;
-        gtk-xft-hintstyle = "hintslight";
+          # Fonts
+          gtk-xft-antialias = 1;
+          gtk-xft-hinting = 1;
+          gtk-xft-hintstyle = "hintslight";
+        };
+        extraCss = ''
+          window { border-radius: 0; }
+        '';
       };
 
-      gtk4.extraConfig = {
-        # Prefer dark theme.
-        gtk-application-prefer-dark-theme = true;
+      gtk4 = {
+        extraConfig = {
+          # Prefer dark theme.
+          gtk-application-prefer-dark-theme = true;
 
-        # Decorations.
-        gtk-decoration-layout = "appmenu:none";
+          # Decorations.
+          gtk-decoration-layout = "appmenu:none";
 
-        # Sounds, again.
-        gtk-error-bell = 0;
-        gtk-enable-event-sounds = 0;
-        gtk-enable-input-feedback-sounds = 0;
+          # Sounds, again.
+          gtk-error-bell = 0;
+          gtk-enable-event-sounds = 0;
+          gtk-enable-input-feedback-sounds = 0;
 
-        # Fonts, you know the drill.
-        gtk-xft-antialias = 1;
-        gtk-xft-hinting = 1;
-        gtk-xft-hintstyle = "hintslight";
+          # Fonts, you know the drill.
+          gtk-xft-antialias = 1;
+          gtk-xft-hinting = 1;
+          gtk-xft-hintstyle = "hintslight";
+        };
+        extraCss = ''
+          window { border-radius: 0; }
+        '';
       };
     };
   };
