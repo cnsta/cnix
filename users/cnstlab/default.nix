1b20a5047bcff205645b349818099aa67a43ccd5
cnst <adam@cnst.dev>
2025-07-02 19:25:33 +0200

    cnixlab git changes

--- users/cnstlab/default.nix
+++ users/cnstlab/default.nix
@@ -29,11 +29,6 @@
       BROWSER = "zen";
       EDITOR = "hx";
       TERM = "xterm-256color";
-
-      # VK_ICD_FILENAMES = "/run/opengl-driver/share/vulkan/icd.d/radeon_icd.x86_64.json";
-      # STEAM_EXTRA_COMPAT_TOOLS_PATHS = "/home/cnst/.steam/root/compatibilitytools.d";
-      # QT_QPA_PLATFORM = "wayland";
-      # XDG_SESSION_TYPE = "wayland";
     };
   };
 
