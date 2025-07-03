c92387806b30d136fbdc4872a8883fe7aebdb8b6
cnst <adam@cnst.dev>
2025-07-03 19:09:47 +0200

    removing ugly comments

--- modules/nixos/programs/hyprland/rules.nix
+++ modules/nixos/programs/hyprland/rules.nix
@@ -15,49 +15,38 @@ in {
     {
       programs.hyprland.settings = {
         windowrule = [
-          # === CALCURSE SETTINGS ===
           "size 843 650, initialTitle:^(floatcal)$"
           "move 100%-w-20 40, initialTitle:^(floatcal)$"
           "float, initialTitle:^(floatcal)$"
 
-          # === TUIRUN SETTINGS ===
           "size 600 300, title:^(tuirun)$"
           "center, title:^(tuirun)$"
-          # "workspace special:tuirun, initialTitle:^(tuirun)$"
           "noborder, title:^(tuirun)$"
           "float, title:^(tuirun)$"
 
-          # === KEEPASSXC SETTINGS ===
           "size 843 530, class:^(org.keepassxc.KeePassXC)$"
           "move 100%-w-20 40, class:^(org.keepassxc.KeePassXC)$"
           "float, class:^(org.keepassxc.KeePassXC)$"
 
-          # === MISSION-CENTER SETTINGS ===
           "size 50% 70%, class:^(io.missioncenter.MissionCenter)$"
           "center, class:^(io.missioncenter.MissionCenter)$"
           "float, class:^(io.missioncenter.MissionCenter)$"
 
-          # === SUPPRESS MAXIMIZE EVENT ===
-          "suppressevent maximize, class:.*" # Suppress maximize events for all windows
+          "suppressevent maximize, class:.*"
 
-          # === NWG-LOOK SETTINGS ===
           "center, class:^(nwg-look)$"
           "float, class:^(nwg-look)$"
 
-          # === OCULANTE SETTINGS ===
           "center, class:^(oculante)$"
           "float, class:^(oculante)$"
 
-          # === PAVUCONTROL SETTINGS ===
           "move 100%-w-20 40, class:^(pavucontrol)$"
           "size 741 585, class:^(pavucontrol)$"
           "float, class:^(pavucontrol)$"
 
-          # === XARCHIVER SETTINGS ===
           "center, class:^(xarchiver)$"
           "float, class:^(xarchiver)$"
 
-          # === FLOATING APPLICATIONS ===
           "float, class:^(org.gnome.FileRoller)$"
           "float, class:^(org.freedesktop.impl.portal.desktop.kde)$"
           "float, class:^(org.corectrl.CoreCtrl)$"
@@ -67,7 +56,6 @@ in {
           "float, class:^(com.github.hluk.copyq)$"
           "float, class:^(blueman-manager)$"
 
-          # === WORKSPACE RULES ===
           "workspace 5 silent, class:^(discord)$"
           "workspace 5 silent, class:^(vesktop)$"
           "workspace 4 silent, class:^(steam_app_0)$"
