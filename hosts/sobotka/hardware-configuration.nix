4880134436ec899afeb3b85ffffcd4f363b83c69
cnst <adam@cnst.dev>
2025-07-13 18:10:21 +0200

    iommu fixes and such

--- hosts/sobotka/hardware-configuration.nix
+++ hosts/sobotka/hardware-configuration.nix
@@ -13,8 +13,8 @@
   ];
 
   boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
-  boot.initrd.kernelModules = [];
-  boot.kernelModules = ["amdgpu"];
+  boot.initrd.kernelModules = ["amdgpu"];
+  boot.kernelModules = [];
   boot.extraModulePackages = [];
 
   fileSystems."/" = {
