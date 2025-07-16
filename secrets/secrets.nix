3e9bdb32fc0c4fdf2354c23e330eb47bced60743
cnst <adam@cnst.dev>
2025-07-16 04:14:52 +0200

    vaultwarden secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,11 +1,14 @@
 let
   cnst = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEUub8vbzUn2f39ILhAJ2QeH8xxLSjiyUuo8xvHGx/VB adam@cnst.dev";
-  cnix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJjoPdpiF8pjKN3ZEHeLEwVxoqwcCdzpVVlZkxJohFdg root@cnix";
+  kima = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJjoPdpiF8pjKN3ZEHeLEwVxoqwcCdzpVVlZkxJohFdg root@cnix";
+  usobotka = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG5ydTeaWcowmNXdDNqIa/lb5l9w5CAzyF2Kg6U5PSSu cnst@sobotka";
+  rsobotka = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJWLTYWowtpGmGolmkCE7+l9jr5QEnDqRxoezNqAIe+j root@nixos";
 in {
-  "cnstssh.age".publicKeys = [cnst cnix];
-  "cnixssh.age".publicKeys = [cnst cnix];
-  "certpem.age".publicKeys = [cnst cnix];
-  "keypem.age".publicKeys = [cnst cnix];
-  "mailpwd.age".publicKeys = [cnst cnix];
-  "gcapi.age".publicKeys = [cnst cnix];
+  "cnstssh.age".publicKeys = [cnst kima];
+  "cnixssh.age".publicKeys = [cnst kima];
+  "certpem.age".publicKeys = [cnst kima];
+  "keypem.age".publicKeys = [cnst kima];
+  "mailpwd.age".publicKeys = [cnst kima];
+  "gcapi.age".publicKeys = [cnst kima];
+  "vaultwarden-env.age".publicKeys = [cnst kima usobotka rsobotka];
 }
