d5dd5217da5e8b90ab388ea3192fffd2326b0642
cnst <adam@cnst.dev>
2025-07-17 17:11:23 +0200

    derp 4

--- modules/server/caddy/default.nix
+++ modules/server/caddy/default.nix
@@ -12,11 +12,6 @@ in {
     server.caddy.enable = mkEnableOption "Enables caddy";
   };
   config = mkIf cfg.enable {
-    age.secrets.cloudflare-env = {
-      file = "${self}/secrets/cloudflare-env.age";
-      owner = "caddy";
-      mode = "400";
-    };
     networking.firewall = let
       ports = [80 443];
     in {
@@ -24,20 +19,20 @@ in {
       allowedUDPPorts = ports;
     };
 
-    # security.acme = {
-    #   acceptTerms = true;
-    #   defaults.email = config.server.email;
-    #   certs.${config.server.domain} = {
-    #     reloadServices = ["caddy.service"];
-    #     domain = "${config.server.domain}";
-    #     extraDomainNames = ["*.${config.server.domain}"];
-    #     dnsProvider = "cloudflare";
-    #     dnsResolver = "1.1.1.1:53";
-    #     dnsPropagationCheck = true;
-    #     group = config.services.caddy.group;
-    #     environmentFile = config.age.secrets.cloudflare-env.path;
-    #   };
-    # };
+    security.acme = {
+      acceptTerms = true;
+      defaults.email = config.server.email;
+      certs.${config.server.domain} = {
+        reloadServices = ["caddy.service"];
+        domain = "${config.server.domain}";
+        extraDomainNames = ["*.${config.server.domain}"];
+        dnsProvider = "cloudflare";
+        dnsResolver = "1.1.1.1:53";
+        dnsPropagationCheck = true;
+        group = config.services.caddy.group;
+        environmentFile = config.age.secrets.cloudflareDnsApiToken.path;
+      };
+    };
 
     services.caddy = {
       enable = true;
