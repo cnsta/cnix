796a02a96626d2f64e144b8b831f4cb74d83eacc
cnst <adam@cnst.dev>
2025-07-19 19:58:51 +0200

    fix syntax

--- modules/home/services/nix-index/default.nix
+++ modules/home/services/nix-index/default.nix
@@ -29,10 +29,10 @@ in {
               coreutils
             ];
             text = ''
-              filename="index-$(uname -m | sed 's/^arm64$/aarch64/')-$(uname | tr '[:upper:]' '[:lower:]')"
+              filename="index-${pkgs.stdenv.system}"
               mkdir -p ~/.cache/nix-index && cd ~/.cache/nix-index
-              wget -q -N https://github.com/nix-community/nix-index-database/releases/latest/download/"$filename"
-              ln -f "$filename" files
+              wget -N "https://github.com/nix-community/nix-index-database/releases/download/latest/$filename"
+              ln -sf "$filename" "files"
             '';
           }
         );
