426fe2845989db835ebded53b058e3d56b735200
cnst <adam@cnst.dev>
2025-07-19 19:51:05 +0200

    syntax fixes

--- modules/home/services/nix-index/default.nix
+++ modules/home/services/nix-index/default.nix
@@ -29,9 +29,9 @@ in {
               coreutils
             ];
             text = ''
-              filename="index-$(uname -m | sed 's/^arm64$/aarch64/')-$(uname | tr [:upper:] [:lower:])"
+              filename="index-$(uname -m | sed 's/^arm64$/aarch64/')-$(uname | tr '[:upper:]' '[:lower:]')"
               mkdir -p ~/.cache/nix-index && cd ~/.cache/nix-index
-              wget -q -N https://github.com/nix-community/nix-index-database/releases/latest/download/$filename
+              wget -q -N https://github.com/nix-community/nix-index-database/releases/latest/download/"$filename"
               ln -f "$filename" files
             '';
           }
