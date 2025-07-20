512afb0bcf005f5ed7655cd38bfbbd8b54c39428
cnst <adam@cnst.dev>
2025-07-20 16:18:18 +0200

    addition to agenix 2

--- modules/nixos/services/agenix/default.nix
+++ modules/nixos/services/agenix/default.nix
@@ -62,7 +62,7 @@ in {
           cloudflareDnsCredentials.file = "${self}/secrets/cloudflareDnsCredentials.age";
           wgCredentials.file = "${self}/secrets/wgCredentials.age";
           wgSobotkaPrivateKey.file = "${self}/secrets/wgSobotkaPrivateKey.age";
-          gutetunEnv.file = "${self}/secrets/gutetunEnv.age";
+          gluetunEnv.file = "${self}/secrets/gluetunEnv.age";
           vaultwardenCloudflared.file = "${self}/secrets/vaultwardenCloudflared.age";
           vaultwarden-env.file = "${self}/secrets/vaultwarden-env.age";
           homepage-env.file = "${self}/secrets/homepage-env.age";
