dd2a8ff4e5a12912c81fd439ac81c6618c769bdb
cnst <adam@cnst.dev>
2025-07-20 10:02:35 +0200

    server variables

--- modules/server/default.nix
+++ modules/server/default.nix
@@ -1,5 +1,10 @@
-{lib, ...}: let
+{
+  lib,
+  config,
+  ...
+}: let
   inherit (lib) mkOption types;
+  cfg = config.server;
 in {
   options.server = {
     email = mkOption {
@@ -15,6 +20,39 @@ in {
       description = ''
         Domain name to be used to access the server services via Caddy reverse proxy
       '';
+    };
+    user = lib.mkOption {
+      default = "share";
+      type = lib.types.str;
+      description = ''
+        User to run the homelab services as
+      '';
+    };
+    group = lib.mkOption {
+      default = "share";
+      type = lib.types.str;
+      description = ''
+        Group to run the homelab services as
+      '';
+    };
+    timeZone = lib.mkOption {
+      default = "Europe/Stockholm";
+      type = lib.types.str;
+      description = ''
+        Time zone to be used for the homelab services
+      '';
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    users = {
+      groups.${cfg.group} = {
+        gid = 993;
+      };
+      users.${cfg.user} = {
+        uid = 994;
+        isSystemUser = true;
+        group = cfg.group;
+      };
     };
   };
 }
