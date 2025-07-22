3404c0070fcac1408177dc803818bc4f5c44c7c2
cnst <adam@cnst.dev>
2025-07-22 21:32:52 +0200

    bunk fixes

--- hosts/bunk/modules.nix
+++ hosts/bunk/modules.nix
@@ -27,6 +27,17 @@
       };
       network = {
         enable = true;
+        extraHosts = ''
+          192.168.88.14 sobotka
+          192.168.88.14 cnst.dev
+          192.168.88.14 lidarr.cnst.dev
+          192.168.88.14 radarr.cnst.dev
+          192.168.88.14 sonarr.cnst.dev
+          192.168.88.14 prowlarr.cnst.dev
+          192.168.88.14 qbt.cnst.dev
+          192.168.88.14 jellyseerr.cnst.dev
+          192.168.88.14 jellyfin.cnst.dev
+        '';
         interfaces = {
           "wlp6s0" = {
             allowedTCPPorts = [22 80 443];
