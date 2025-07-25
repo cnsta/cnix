0bcf4511006766dc45e27a25082f22795a801228
cnst <adam@cnst.dev>
2025-07-25 16:52:06 +0200

    extrahosts sobotka 2

--- hosts/sobotka/modules.nix
+++ hosts/sobotka/modules.nix
@@ -29,7 +29,18 @@
       network = {
         enable = true;
         extraHosts = ''
+          192.168.88.14 sobotka
           192.168.88.14 cnst.dev
+          192.168.88.14 lidarr.cnst.dev
+          192.168.88.14 radarr.cnst.dev
+          192.168.88.14 sonarr.cnst.dev
+          192.168.88.14 prowlarr.cnst.dev
+          192.168.88.14 bazarr.cnst.dev
+          192.168.88.14 qbt.cnst.dev
+          192.168.88.14 jellyseerr.cnst.dev
+          192.168.88.14 jellyfin.cnst.dev
+          192.168.88.14 uptime.cnst.dev
+          192.168.88.14 pihole.cnst.dev
         '';
         interfaces = {
           "enp6s0" = {
