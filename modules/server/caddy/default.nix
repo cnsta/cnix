1e675d1c818fbb301083e1f90418fd07788da295
cnst <adam@cnst.dev>
2025-09-15 08:18:35 +0200

    feat(dashy): broken

--- modules/server/caddy/default.nix
+++ modules/server/caddy/default.nix
@@ -2,43 +2,37 @@
   config,
   lib,
   ...
-}:
-let
+}: let
   inherit (lib) mkIf mkEnableOption;
   cfg = config.server.caddy;
 
-  getCloudflareCredentials =
-    hostname:
-    if hostname == "ziggy" then
-      config.age.secrets.cloudflareDnsCredentialsZiggy.path
-    else if hostname == "sobotka" then
-      config.age.secrets.cloudflareDnsCredentials.path
-    else
-      throw "Unknown hostname: ${hostname}";
-  in      
-{
+  getCloudflareCredentials = hostname:
+    if hostname == "ziggy"
+    then config.age.secrets.cloudflareDnsCredentialsZiggy.path
+    else if hostname == "sobotka"
+    then config.age.secrets.cloudflareDnsCredentials.path
+    else throw "Unknown hostname: ${hostname}";
+in {
   options = {
     server.caddy.enable = mkEnableOption "Enables caddy";
   };
   config = mkIf cfg.enable {
-    networking.firewall =
-      let
-        ports = [
-          80
-          443
-        ];
-      in
-      {
-        allowedTCPPorts = ports;
-      };
+    networking.firewall = let
+      ports = [
+        80
+        443
+      ];
+    in {
+      allowedTCPPorts = ports;
+    };
 
     security.acme = {
       acceptTerms = true;
       defaults.email = config.server.email;
       certs.${config.server.domain} = {
-        reloadServices = [ "caddy.service" ];
+        reloadServices = ["caddy.service"];
         domain = "${config.server.domain}";
-        extraDomainNames = [ "*.${config.server.domain}" ];
+        extraDomainNames = ["*.${config.server.domain}"];
         dnsProvider = "cloudflare";
         dnsResolver = "1.1.1.1:53";
         dnsPropagationCheck = true;
