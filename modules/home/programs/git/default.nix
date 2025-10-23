500c7105ae5d3f561b25a91c2efdb36a4be1f6c4
cnst <adam@cnst.dev>
2025-10-23 19:13:12 +0200

    chore(git): fix deprecated settings

--- modules/home/programs/git/default.nix
+++ modules/home/programs/git/default.nix
@@ -15,47 +15,54 @@ in
   };
   config = mkIf cfg.enable {
     home.packages = [ pkgs.gh ];
-    programs.git = {
-      enable = true;
-      userName = osConfig.settings.accounts.username;
-      userEmail = osConfig.settings.accounts.mail;
+    programs = {
+      git = {
+        enable = true;
+        settings = {
+          # user.signingkey = "${config.home.homeDirectory}/.ssh/id_ed25519.pub";
+          user = {
+            name = osConfig.settings.accounts.username;
+            email = osConfig.settings.accounts.mail;
+            signingkey = "${config.home.homeDirectory}/.config/git/allowed_signers";
+          };
+          signing = {
+            format = lib.mkDefault "ssh";
+            key = "${config.home.homeDirectory}/.ssh/id_ed25519";
+            signByDefault = true;
+          };
+          gpg = {
+            # format = lib.mkDefault "ssh";
+            ssh.allowedSignersFile =
+              config.home.homeDirectory + "/" + config.xdg.configFile."git/allowed_signers".target;
+          };
+          commit = {
+            verbose = true;
+            gpgSign = false;
+          };
+          init.defaultBranch = "main";
+          merge.conflictStyle = "diff3";
+          diff.algorithm = "histogram";
+          log.date = "iso";
+          column.ui = "auto";
+          branch.sort = "committerdate";
+          push.autoSetupRemote = true;
+          rerere.enabled = true;
+        };
+        lfs.enable = true;
+        ignores = [
+          ".direnv"
+          "result"
+          ".jj"
+        ];
+      };
+
       delta = {
+        enableGitIntegration = true;
         enable = true;
         options.dark = true;
       };
-      extraConfig = {
-        # user.signingkey = "${config.home.homeDirectory}/.ssh/id_ed25519.pub";
-        user.signingkey = "${config.home.homeDirectory}/.config/git/allowed_signers";
-        signing = {
-          format = lib.mkDefault "ssh";
-          key = "${config.home.homeDirectory}/.ssh/id_ed25519";
-          signByDefault = true;
-        };
-        gpg = {
-          # format = lib.mkDefault "ssh";
-          ssh.allowedSignersFile =
-            config.home.homeDirectory + "/" + config.xdg.configFile."git/allowed_signers".target;
-        };
-        commit = {
-          verbose = true;
-          gpgSign = false;
-        };
-        init.defaultBranch = "main";
-        merge.conflictStyle = "diff3";
-        diff.algorithm = "histogram";
-        log.date = "iso";
-        column.ui = "auto";
-        branch.sort = "committerdate";
-        push.autoSetupRemote = true;
-        rerere.enabled = true;
-      };
-      lfs.enable = true;
-      ignores = [
-        ".direnv"
-        "result"
-        ".jj"
-      ];
     };
+
     xdg.configFile."git/allowed_signers".text = ''
       ${osConfig.settings.accounts.mail} namespaces="git" ${osConfig.settings.accounts.sshKey}
     '';
