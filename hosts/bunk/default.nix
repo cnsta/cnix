6110dccaa0edaf8aec2c44adc8507016d343dbee
cnst <adam@cnst.dev>
2025-10-28 19:25:34 +0100

    bunk things

--- hosts/bunk/default.nix
+++ hosts/bunk/default.nix
@@ -38,17 +38,11 @@ in
     ./settings.nix
   ];
 
+  boot.initrd.luks.devices."luks-0ad53967-bb38-4485-be75-ca55ae4c3b68".device = "/dev/disk/by-uuid/0ad53967-bb38-4485-be75-ca55ae4c3b68";
   networking.hostName = "bunk";
-
-  swapDevices = [
-    {
-      device = "/var/lib/swapfile";
-      size = 32 * 1024;
-    }
-  ];
 
   environment.variables.NH_FLAKE = "/home/cnst/.nix-config";
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  system.stateVersion = lib.mkDefault "23.11";
+  system.stateVersion = lib.mkDefault "25.05";
 }
