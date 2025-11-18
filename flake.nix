b87451c3ccd45bf6e7ad19b36280d30d0cdd80dd
cnst <adam@cnst.dev>
2025-11-18 19:55:43 +0100

    tweak(zen): sync to beta branch

--- flake.nix
+++ flake.nix
@@ -121,7 +121,7 @@
     };
 
     zen-browser = {
-      url = "github:0xc000022070/zen-browser-flake";
+      url = "github:0xc000022070/zen-browser-flake/beta";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
