c0791acb5bbc9b206289b243a87c5cb73d09b023
cnst <adam@cnst.dev>
2026-02-22 18:24:16 +0100

    fix(homepage-dashboard): fix warning

--- modules/server/services/homepage/default.nix
+++ modules/server/services/homepage/default.nix
@@ -29,7 +29,7 @@ in
       };
       homepage-dashboard = {
         enable = true;
-        environmentFile = config.age.secrets.homepageEnvironment.path;
+        environmentFiles = config.age.secrets.homepageEnvironment.path;
 
         settings = {
           color = "stone";
