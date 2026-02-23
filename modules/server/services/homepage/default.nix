dc40547e464e34e5aa7ed7e40b05d27585468d4a
cnst <adam@cnst.dev>
2026-02-23 16:25:30 +0100

    fix(homepage): put environmentfiles in list

--- modules/server/services/homepage/default.nix
+++ modules/server/services/homepage/default.nix
@@ -29,7 +29,7 @@ in
       };
       homepage-dashboard = {
         enable = true;
-        environmentFiles = config.age.secrets.homepageEnvironment.path;
+        environmentFiles = [ config.age.secrets.homepageEnvironment.path ];
 
         settings = {
           color = "stone";
