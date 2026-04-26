ce3aee8e89e35f4888b9531b10590a2577db8eea
cnst <adam@cnst.dev>
2026-04-26 16:45:08 +0200

    revert(nixpkgs): remove overlay fix

--- system/nixpkgs.nix
+++ system/nixpkgs.nix
@@ -10,13 +10,6 @@
     };
 
     overlays = [
-      # Skipping tests while upstream sorts it out, revert once
-      # Hydra consistently builds openldap green.
-      (final: prev: {
-        openldap = prev.openldap.overrideAttrs (_: {
-          doCheck = false;
-        });
-      })
     ];
   };
 }
