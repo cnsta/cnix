2c0c468f105eec3ec95437c2cd8cbe27a4000503
cnst <adam@cnst.dev>
2026-05-10 20:55:21 +0200

    feat(fish): consolidate into config.fish, /etc/fish overrode aliases

--- modules/programs/fish/default.nix
+++ modules/programs/fish/default.nix
@@ -8,15 +8,7 @@ with lib;
 let
   cfg = config.cnix.programs.fish;
   acct = config.cnix.settings.accounts;
-  common = import ./common.nix;
-
-  ezaPkgs = [ pkgs.eza ];
-  ezaInit = ''
-    if command -q eza
-        alias ls eza
-        alias tree 'eza --tree --icons=always'
-    end
-  '';
+  common = import ./common.nix { inherit pkgs lib; };
 
   abbrsToFish =
     abbrs: concatStringsSep "\n" (mapAttrsToList (n: v: "abbr -a ${n} -- ${escapeShellArg v}") abbrs);
@@ -46,9 +38,10 @@ in
     }
 
     (mkIf cfg.homeless.enable {
-      environment.systemPackages = ezaPkgs ++ [
-        pkgs.carapace
-        pkgs.starship
+      environment.systemPackages = with pkgs; [
+        eza
+        carapace
+        starship
       ];
       programs.starship.enable = true;
       programs.fish = {
@@ -56,7 +49,6 @@ in
         shellAliases = common.aliases;
         interactiveShellInit = ''
           ${common.interactiveInit}
-          ${ezaInit}
           ${getExe pkgs.carapace} _carapace fish | source
         '';
       };
@@ -64,41 +56,26 @@ in
 
     (mkIf hasUsers {
       hjem.users = genAttrs acct.defaultUsers (_: {
-        packages = ezaPkgs ++ [
-          pkgs.carapace
-          pkgs.starship
+        packages = with pkgs; [
+          eza
+          carapace
+          starship
         ];
-
         xdg.config.files = {
-          "fish/conf.d/00-abbrs.fish".text = abbrsToFish common.abbrs;
-          "fish/conf.d/01-aliases.fish".text = aliasesToFish common.aliases;
-
-          "fish/conf.d/02-interactive.fish".text = ''
+          "fish/config.fish".text = ''
+            ${abbrsToFish common.abbrs}
+            ${aliasesToFish common.aliases}
             if status is-interactive
             ${common.interactiveInit}
-            ${ezaInit}
-            end
-          '';
-
-          "fish/conf.d/03-starship.fish".text = ''
-            if status is-interactive
                 starship init fish | source
-            end
-          '';
-
-          "fish/conf.d/04-carapace.fish".text = ''
-            if status is-interactive
                 carapace _carapace fish | source
             end
           '';
-
           "fish/functions/up-or-search.fish".source = ./up-or-search.fish;
-
           "fish/functions/fish_greeting.fish".text = ''
             function fish_greeting
             end
           '';
-
           "fish/functions/nix-inspect.fish".text = ''
             function nix-inspect
                 set -s PATH | grep "PATH\[.*/nix/store" | cut -d '|' -f2 \
