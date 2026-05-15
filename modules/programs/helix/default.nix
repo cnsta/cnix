58684ed44baebce960ca6d257f48cb9d684ed764
cnst <adam@cnst.dev>
2026-05-15 14:28:33 +0200

    feat(misc): hyprlock/idle tweaks, helix and dev pkgs refactor

--- modules/programs/helix/default.nix
+++ modules/programs/helix/default.nix
@@ -1,4 +1,3 @@
-# modules/programs/helix/default.nix
 {
   config,
   lib,
@@ -15,18 +14,46 @@ let
     ;
   cfg = config.cnix.programs.helix;
   acct = config.cnix.settings.accounts;
-
   helixPkg = inputs.helix-flake.packages.${pkgs.stdenv.hostPlatform.system}.default;
+
+  rustPkgs = pkgs.extend inputs.rust-overlay.overlays.default;
+  rustToolchain = rustPkgs.rust-bin.stable.latest.default.override {
+    extensions = [
+      "rust-analyzer"
+      "rust-src"
+    ];
+  };
+
+  languageServers = with pkgs; [
+    bash-language-server
+    clang-tools
+    fish-lsp
+    lua-language-server
+    markdown-oxide
+    nixd
+    phpactor
+    typescript-language-server
+    vscode-langservers-extracted
+    kdePackages.qtdeclarative
+  ];
+
+  formatters = with pkgs; [
+    deno
+    nixfmt
+    prettier
+    shfmt
+    stylua
+  ];
+
+  languagePkgs = [ rustToolchain ] ++ languageServers ++ formatters;
 in
 {
   options.cnix.programs.helix.enable = mkEnableOption "the Helix editor";
-
   config = mkIf cfg.enable (mkMerge [
     {
-      environment.systemPackages = [ helixPkg ];
+      environment.systemPackages = [ helixPkg ] ++ languagePkgs;
       environment.variables.EDITOR = "hx";
     }
-
     (mkIf (acct.defaultUsers != [ ]) {
       hjem.users = genAttrs acct.defaultUsers (_: {
         xdg.config.files = {
