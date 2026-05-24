c1bf12b3564d2772c548f60ba27da92fa391a943
cnst <adam@cnst.dev>
2026-05-24 17:16:11 +0200

    tweak(pihole): fix ziggy config

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,25 +1,19 @@
 let
-  # --- Users ---
   ukima = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFmCpN9bDtv7Wr4MYn3zf10yivAENDynFTq0y3M+c85X cnst@kima";
   rkima = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHUx5j5tTgRMmLB/DC1nmRdPeNZC04UQiF3aDowhd+kn root@nixos";
 
-  # --- Hosts: bunk ---
   ubunk = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHd48tFwqAKAoE+37zDsTGQTrBS/6cQ+kQQw3596XsdY cnst@bunk";
   rbunk = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGwhIj0kRDt3WbpBPF/JbQsrLWIVGWjfz78p8L0ij16M root@bunk";
 
-  # --- Hosts: sobotka ---
   usobotka = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG5ydTeaWcowmNXdDNqIa/lb5l9w5CAzyF2Kg6U5PSSu cnst@sobotka";
   rsobotka = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJWLTYWowtpGmGolmkCE7+l9jr5QEnDqRxoezNqAIe+j root@nixos";
 
-  # --- Hosts: ziggy ---
   uziggy = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICtL8uBsJ3UL4+scqjEcyXYQOVlKziJk9YJ78YP6jCxq cnst@nixos";
   rziggy = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHnca8xg1MZ4Hx5k5SVFSxcPnWc1O6r7w7JGYzX9aQm8 root@nixos";
 
-  # --- Hosts: toothpc ---
   utoothpc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGe3s7WbaM0aZTYHCE1ugiG/SxFXLSbWcLAWceFotpuh toothpick@nixos";
   rtoothpc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOzjuaOX94oRwWVsRjE4mo5QTw35lEmFKyHtlh2XCTBg root@toothpc";
 
-  # --- Groups ---
   kima = [
     ukima
     rkima
@@ -44,7 +38,7 @@ let
   all = kima ++ bunk ++ sobotka ++ ziggy ++ toothpc;
 in
 {
-  # Sobotka-specific
+  # sobotka-specific
   "cloudflareEnvironment.age".publicKeys = sobotka;
   "vaultwardenEnvironment.age".publicKeys = sobotka;
   "homepageEnvironment.age".publicKeys = sobotka;
@@ -105,10 +99,10 @@ in
   "minifluxPgPwd.age".publicKeys = sobotka;
   "harmoniaSignKey.age".publicKeys = sobotka;
 
-  # Ziggy-specific
+  # ziggy-specific
   "cloudflareDnsCredentialsZiggy.age".publicKeys = ziggy;
   "ziggyPihole.age".publicKeys = ziggy;
 
-  # Both sobotka + ziggy (for HA stuff like keepalived)
+  # sobotka + ziggy
   "keepalived.age".publicKeys = sobotka ++ ziggy;
 }
