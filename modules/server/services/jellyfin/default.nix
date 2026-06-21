ca50e2899dc0bf59acf3d516095bdbf2fb50169d
cnst <adam@cnst.dev>
2026-06-21 15:00:10 +0200

    fix(jellyfin): container

--- modules/server/services/jellyfin/default.nix
+++ modules/server/services/jellyfin/default.nix
@@ -1,21 +1,38 @@
 {
   config,
   lib,
+  self,
   pkgs,
   ...
-}:
-let
+}: let
   unit = "jellyfin";
-  cfg = config.cnix.server.services.${unit};
   srv = config.cnix.server;
-in
-{
-  config = lib.mkIf cfg.enable {
-    services.${unit} = {
-      enable = true;
-      user = srv.user;
-      group = srv.group;
+  cfg = config.cnix.server.services.${unit};
+in {
+  config = lib.mkIf (srv.infra.podman.enable && cfg.enable) {
+    age.secrets = {
+      jellyfinEnvironment.file = self + "/secrets/jellyfinEnvironment.age";
     };
+
+    virtualisation.oci-containers.containers = {
+      ${unit} = {
+        image = "ghcr.io/hotio/jellyfin:latest";
+        autoStart = true;
+        ports = ["${toString cfg.port}:8096"];
+        volumes = [
+          "/var/lib/jellyfin:/config"
+          "/var/cache/jellyfin:/cache"
+          "/mnt/data/media/series:/series:ro"
+          "/mnt/data/media/films:/films:ro"
+        ];
+        extraOptions = [
+          "--device=/dev/dri/renderD129"
+          "--network=host"
+        ];
+        environmentFiles = [config.age.secrets.jellyfinEnvironment.path];
+      };
+    };
+
     environment.systemPackages = with pkgs; [
       jellyfin-ffmpeg
     ];
